bf27621623a0ae5930a4cfc7a24a04d8a2c2a0a1
javydekoning <javydekoning+github@gmail.com>
2026-02-16 09:06:14 +0100

    Updating fzf cd hotkey

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,4 +1,4 @@
-# fzf key bindings (Ctrl+R history, Ctrl+T file, Alt+C cd) and completion
+# fzf key bindings (Ctrl+R history, Ctrl+T file, Esc+C cd) and completion
 # Symlinked from dotfiles; sources from platform-specific fzf install path.
 
 if [[ -f "$(brew --prefix 2>/dev/null)/opt/fzf/shell/key-bindings.zsh" ]]; then
@@ -11,3 +11,8 @@ elif [[ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]]; then
   source /usr/share/doc/fzf/examples/key-bindings.zsh
   [[ -f /usr/share/doc/fzf/examples/completion.zsh ]] && source /usr/share/doc/fzf/examples/completion.zsh
 fi
+
+# Cd-into-directory: use Esc then c (fzf default \ec is this sequence)
+bindkey -M emacs '\ec' fzf-cd-widget
+bindkey -M vicmd '\ec' fzf-cd-widget
+bindkey -M viins '\ec' fzf-cd-widget
